bee97d9bab393a6d3f646596ef15b0ad5338538d
cjaytechguy <cjaytechguy@gmail.com>
2020-06-22 09:39:49 +0100

    finished the sign in with email and password functionality

--- src/components/signin/SignIn.jsx
+++ src/components/signin/SignIn.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './SignIn.scss';
 import FormInput from '../form-input/FormInput';
 import CustomButton from '../custom-button/CustomButton';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
 
 class SignIn extends Component {
   constructor() {
@@ -13,9 +13,18 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault();
-    this.setState({ email: '', password: '' });
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+
+      this.setState({ email: '', password: '' });
+    } catch (e) {
+      console.log('Error signing in', e.message);
+    }
   };
 
   handleChange = (e) => {
