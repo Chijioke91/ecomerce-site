05a45f09d744382d307d58670d05e1fc156f511b
cjaytechguy <cjaytechguy@gmail.com>
2020-06-25 20:40:27 +0100

    persisted the reducer and store using redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,14 @@
 import { createStore } from 'redux';
+import { persistStore } from 'redux-persist';
 import reducers from '../redux/rootReducer';
 
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
-const store = createStore(
+export const store = createStore(
   reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-export default store;
+export const persistor = persistStore(store);
